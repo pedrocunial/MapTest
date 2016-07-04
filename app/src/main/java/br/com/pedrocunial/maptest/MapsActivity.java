117906006bb4c49b8a16e8da53e612d2086770bc
pedrocunial <pedrocc4@al.insper.edu.br>
2016-07-04 20:41:29 -0300

    Fiz um GPS! O código ainda está uma porquice, mas estou exausto, vou ter que deixar a limpeza para outra hora.

--- app/src/main/java/br/com/pedrocunial/maptest/MapsActivity.java
+++ app/src/main/java/br/com/pedrocunial/maptest/MapsActivity.java
@@ -11,13 +11,19 @@ import android.location.Location;
 import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.Places;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -39,19 +45,24 @@ import java.util.Random;
 import static br.com.pedrocunial.maptest.model.JSONParser.getJSONFromUrl;
 import static br.com.pedrocunial.maptest.model.PathGoogleMap.makeURL;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
 
     private GoogleMap       mMap;
     private Random          random;
     private LatLng          home;
     private LatLng          insper;
     private LatLng          cesar;
-    private LatLng          position;
-    private LocationManager locationManager;
     private LocationRequest mLocationRequest;
+    private GoogleApiClient mGoogleApiClient;
+    private Thread          routeTracer;
 
-    private final int             SHORT_INTERVAL = 1000;
-    private final int             LONG_INTERVAL  = 5000;
+    private final String TAG           = "MapApp";
+    private final int    LONG_INTERVAL = 5000;
+    private final int    ZOOM          = 17;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,10 +73,34 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        LocationRequest mLocationRequest = new LocationRequest();
-        mLocationRequest.setInterval(LONG_INTERVAL);
-        mLocationRequest.setFastestInterval(SHORT_INTERVAL);
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        random = new Random();
+
+        Log.i(TAG, String.valueOf(ZOOM));
+
+        buildGoogleApiClient();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Connect to the client
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        // Disconnecting from the client
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    private void buildGoogleApiClient() {
+        // Starts the google api client (which is an assynchronous task
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
     }
 
     /**
@@ -79,80 +114,32 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap   = googleMap;
-        random = new Random();
-
-        double[] homeLatLng = this.getLatLongFromPlace("Rua Gomes de Carvalho, 638");
-        home = new LatLng(homeLatLng[0], homeLatLng[1]);
-        mMap.addMarker(new MarkerOptions().position(home).title("Home"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(home));
-        mMap.moveCamera(CameraUpdateFactory.zoomTo(17));
-
-        double[] insperLatLng = this.getLatLongFromPlace("Rua Quata, 300");
-        insper = new LatLng(insperLatLng[0], insperLatLng[1]);
-        mMap.addMarker(new MarkerOptions().position(insper).title("Insper"));
+        mMap = googleMap;
 
         double[] cesarLatLng = this.getLatLongFromPlace("CESAR - Recife");
         cesar = new LatLng(cesarLatLng[0], cesarLatLng[1]);
         mMap.addMarker(new MarkerOptions().position(cesar).title("C.E.S.A.R"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(cesar));
 
+        double[] brumLatLng = this.getLatLongFromPlace("Rua do Brum 77 - Recife");
+        LatLng brum = new LatLng(brumLatLng[0], brumLatLng[1]);
+        mMap.addMarker(new MarkerOptions().position(brum).title("Brum"));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(ZOOM));
 
-        // Create a criteria object to retrieve provider
-        Criteria criteria = new Criteria();
-
-        // Get the name of the best provider
-        String provider = locationManager.getBestProvider(criteria, true);
-
-        // Get Current Location
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
-                PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        Location myLocation = locationManager.getLastKnownLocation(provider);
-
-        // Get latitude of the current location
-        double latitude = myLocation.getLatitude();
-
-        // Get longitude of the current location
-        double longitude = myLocation.getLongitude();
-
-        // Create a LatLng object for the current location
-        LatLng position = new LatLng(latitude, longitude);
-
-        // Show the current location in Google Map
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(position));
-        mMap.addMarker(new MarkerOptions().position(position).title("You"));
-
-        String url = makeURL(position, cesar);
-//        new connectAsyncTask(url).execute();
-
-//        String url      = makeURL(home, insper);
-//        String urlVolta = makeURL(insper, home);
-//        new connectAsyncTask(url).execute();
-//        new connectAsyncTask(urlVolta).execute();
+        String url = makeURL(brum, cesar);
+        new connectAsyncTask(url).execute();
     }
 
     public double[] getLatLongFromPlace(String place) {
         double[] latLng = new double[2];
 
         try {
-            Geocoder selectedPlaceGeocoder = new Geocoder(this);
+            Geocoder                       selectedPlaceGeocoder = new Geocoder(this);
             List<android.location.Address> address;
 
             address = selectedPlaceGeocoder.getFromLocationName(place, 5);
 
-            if (address == null) {
-                System.out.println("ops!");
-            } else {
+            if (address != null) {
                 android.location.Address location = address.get(0);
                 latLng[0] = location.getLatitude();
                 latLng[1] = location.getLongitude();
@@ -169,20 +156,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         try {
             //Tranform the string into a json object
-            final JSONObject json        = new JSONObject(result);
-            JSONArray routeArray         = json.getJSONArray("routes");
-            JSONObject routes            = routeArray.getJSONObject(0);
-            JSONObject overviewPolylines = routes.getJSONObject("overview_polyline");
+            final JSONObject json              = new JSONObject(result);
+            JSONArray        routeArray        = json.getJSONArray("routes");
+            JSONObject       routes            = routeArray.getJSONObject(0);
+            JSONObject       overviewPolylines = routes.getJSONObject("overview_polyline");
 
-            String encodedString         = overviewPolylines.getString("points");
-            List<LatLng> list = decodePoly(encodedString);
+            String       encodedString = overviewPolylines.getString("points");
+            List<LatLng> list          = decodePoly(encodedString);
 
-            System.out.println("Hello");
-            for(int z = 0; z < list.size()-1; z++){
-                LatLng src= list.get(z);
-                LatLng dest= list.get(z+1);
+            for (int z = 0; z < list.size() - 1; z++) {
+                LatLng src  = list.get(z);
+                LatLng dest = list.get(z + 1);
                 Polyline line = mMap.addPolyline(new PolylineOptions()
-                        .add(new LatLng(src.latitude, src.longitude), new LatLng(dest.latitude,   dest.longitude))
+                        .add(new LatLng(src.latitude, src.longitude),
+                                new LatLng(dest.latitude, dest.longitude))
                         .width(4)
                         .color(color).geodesic(true));
             }
@@ -193,9 +180,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private List<LatLng> decodePoly(String encoded) {
 
-        List<LatLng> poly = new ArrayList<LatLng>();
-        int index = 0, len = encoded.length();
-        int lat = 0, lng = 0;
+        List<LatLng> poly  = new ArrayList<LatLng>();
+        int          index = 0, len = encoded.length();
+        int          lat   = 0, lng = 0;
 
         while (index < len) {
             int b, shift = 0, result = 0;
@@ -203,7 +190,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             do {
                 b = encoded.charAt(index++) - 63;
                 result |= (b & 0x1f) << shift;
-                shift  += 5;
+                shift += 5;
             } while (b >= 0x20);
 
             int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
@@ -219,8 +206,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
             lng += dlng;
 
-            LatLng p = new LatLng( (((double) lat / 1E5)),
-                    (((double) lng / 1E5) ));
+            LatLng p = new LatLng((((double) lat / 1E5)),
+                    (((double) lng / 1E5)));
             poly.add(p);
         }
 
@@ -232,10 +219,49 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         double lat = location.getLatitude();
         double lng = location.getLongitude();
 
+        Log.i(TAG, "Starting...");
+
         LatLng position = new LatLng(lat, lng);
 
-        String url = makeURL(position, cesar);
-        new connectAsyncTask(url);
+        mMap.addMarker(new MarkerOptions().position(position).title("You!"));
+        mMap.animateCamera(CameraUpdateFactory.newLatLng(position));
+
+        String url  = makeURL(position, cesar);
+        new connectAsyncTaskWithoutAlert(url).execute();
+        Log.i(TAG, "Complete!");
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        mLocationRequest = LocationRequest.create();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(LONG_INTERVAL); // Update location every five seconds
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest,
+                this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i(TAG, "GoogleApiClient connection has been suspend");
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.i(TAG, "GoogleApiClient connection has failed");
+
     }
 
     private class connectAsyncTask extends AsyncTask<Void, Void, String> {
@@ -269,4 +295,31 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         }
     }
+
+    private class connectAsyncTaskWithoutAlert extends AsyncTask<Void, Void, String> {
+        String url;
+
+        connectAsyncTaskWithoutAlert(String urlPass) {
+            url = urlPass;
+        }
+        @Override
+        protected void onPreExecute() {
+            // TODO Auto-generated method stub
+            super.onPreExecute();
+        }
+        @Override
+        protected String doInBackground(Void... params) {
+            return getJSONFromUrl(url);
+        }
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            if(result != null) {
+                int color = random.nextInt(0xFFFFFF) + 0xFF000000;
+                drawPath(result, color);
+            }
+        }
+    }
+
 }
