495c4f9a31a3abfd7341347a1a87a4fa19f88d6b
cflavs <cflavs.7@gmail.com>
2016-07-22 12:56:13 -0300

    Comunicação primária da API com o app

--- app/src/main/java/br/com/pedrocunial/maptest/LoginActivity.java
+++ app/src/main/java/br/com/pedrocunial/maptest/LoginActivity.java
@@ -7,6 +7,7 @@ import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.view.ViewCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
@@ -20,6 +21,7 @@ public class LoginActivity extends AppCompatActivity {
     EditText username;
     EditText password;
     Button entrar_btn;
+    boolean flag;
     private static final String EXTRA_IMAGE = "br.com.pedrocunial.maptest.extraImage";
     private static final String EXTRA_TITLE = "br.com.pedrocunial.maptest.extraTitle";
     private CollapsingToolbarLayout collapsingToolbarLayout;
@@ -44,8 +46,37 @@ public class LoginActivity extends AppCompatActivity {
         entrar_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                final String file_url = "http://192.168.43.208:3000/login";
+                final String name = username.getText().toString();
+                final String pass = password.getText().toString();
+                final String[] userData = {name, pass, file_url};
+
+                LoginRequestActivity asyncTask =new LoginRequestActivity(new LoginRequestActivity.AsyncResponse() {
+                    @Override
+                    public void processFinish(Object output) {
+                        Log.d("Response", (String) output);
+                        if(output.equals("ok")){
+                            Toast.makeText(getApplicationContext(), "Login...",Toast.LENGTH_SHORT).show();
+                            Intent it = new Intent(LoginActivity.this, MapsActivity.class);
+                            startActivity(it);
+                        }else{
+                            Toast.makeText(getApplicationContext(), "Usuário ou senha incorreto, tente novamente",Toast.LENGTH_SHORT).show();
+                            Intent itStatus = new Intent(LoginActivity.this,StatusActivity.class);
+                            startActivity(itStatus);
+                        }
+
+                    }
+                });
+                asyncTask.execute(userData);
+            //    LoginRequestActivity httpRequest = new LoginRequestActivity();
+             //   String status="falhou";
+              //  new LoginRequestActivity().execute(userData);
+               // System.out.println(status);
+                //if(status.equals("ok")){
+                 //   System.out.println("autenticado");
+               // }
                 //Checks Authentication
-                if(username.getText().toString().equals("admin")&&password.getText().toString().equals("12345")){
+                /*if(username.getText().toString().equals("admin")&&password.getText().toString().equals("12345")){
                     Toast.makeText(getApplicationContext(), "Login...",Toast.LENGTH_SHORT).show();
                     Intent it = new Intent(LoginActivity.this, MapsActivity.class);
                     startActivity(it);
@@ -54,7 +85,7 @@ public class LoginActivity extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(), "Usuário ou senha incorreto, tente novamente",Toast.LENGTH_SHORT).show();
                     Intent itStatus = new Intent(LoginActivity.this,StatusActivity.class);
                     startActivity(itStatus);
-                }
+                }*/
             }
         });
     }
---
+++ app/src/main/java/br/com/pedrocunial/maptest/LoginInterface.java
@@ -0,0 +1,14 @@
+package br.com.pedrocunial.maptest;
+
+import android.content.Context;
+import android.content.Intent;
+
+import com.google.android.gms.maps.GoogleMap;
+
+/**
+ * Created by summerjob on 05/07/16.
+ */
+public interface LoginInterface {
+    void processFinish(String output);
+
+}
